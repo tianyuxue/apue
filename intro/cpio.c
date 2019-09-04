05cd58d044b6a9f3494e6a5d3b5f92cfd36ecef1
jitianyu <jitianyu@htyunwang.com>
2019-09-04 19:44:43 +0800

    add copy stdin to stdout

---
+++ intro/cpio.c
@@ -0,0 +1,23 @@
+#include "apue.h"
+
+#define BUFFERSIZE 4096
+
+int
+main(void)
+{
+    int n;
+    char buf[BUFFERSIZE];
+
+    while ((n = read(STDIN_FILENO, buf, BUFFERSIZE)) > 0)
+    {
+        if (write(STDOUT_FILENO, buf, n) != n) {
+            err_sys("write error");
+        }
+    }
+
+    if (n < 0) {
+        err_sys("read error");
+    }
+    
+    exit(0);
+}
